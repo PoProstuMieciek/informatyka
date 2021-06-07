d5d05a0a01cde0497f04c0b7c4a7bb3bf0aae6fc
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-07 12:38:58 +0200

    Add sqrt and cbrt functions to Evaluator in 'calc' solution

--- calc/src/evaluator/Evaluator.cpp
+++ calc/src/evaluator/Evaluator.cpp
@@ -90,6 +90,8 @@ double Evaluator::evaluate_function(string function_name, double arg1)
     if (function_name == "pi") return M_PI;
     if (function_name == "rad") return arg1 * (M_PI / 180.0);
     if (function_name == "deg") return arg1 * (180.0 / M_PI);
+    if (function_name == "sqrt") return sqrt(arg1);
+    if (function_name == "cbrt") return cbrt(arg1);
 
     printf("[ERROR] Invalid function call. Function with name '%s' does not exist.\n", function_name.c_str());
     exit(1);
