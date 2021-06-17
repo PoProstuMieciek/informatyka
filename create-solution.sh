f9fae3aee315329fdca9b05b9bb44627765fcd54
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-17 21:27:33 +0200

    Modify `create-solution.sh` to auto-create main.cpp and tests

--- create-solution.sh
+++ create-solution.sh
@@ -3,8 +3,10 @@ name=$1
 mkdir $name/
 cd $name/
 
-mkdir src/
-touch src/main.cpp
+mkdir src/ test/
 
+touch test/example.{in,out}
 cp ../Makefile.template ./Makefile
+cp ../main.cpp.template ./src/main.cpp
+
 sed -i "s/SOLUTION_NAME/${name,,}/" Makefile
