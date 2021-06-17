351c6cd1d0972b85121161f009a53bdd66bb14f2
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-17 21:24:55 +0200

    Add recursion to `nth_triangle_number`

--- nth_triangle_number/src/main.cpp
+++ nth_triangle_number/src/main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 long long nth_triangle_number(int x)
 {
@@ -10,9 +11,18 @@ long long nth_triangle_number(int x)
     return output;
 }
 
+long long nth_triangle_number_recursion(int x)
+{
+    if (x == 1) return x;
+    else return x + nth_triangle_number_recursion(x - 1);
+}
+
 int main()
 {
     int x;
-    scanf("%i", &x);
-    printf("%lli\n", nth_triangle_number(x));
+    cin >> x;
+
+    // you can see that this returns the same value no matter what we use (iteration or recursion)
+    cout << nth_triangle_number(x) << endl;
+    cout << nth_triangle_number_recursion(x) << endl;
 }
