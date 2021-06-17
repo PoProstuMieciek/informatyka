6796c10f1813735604d59316c3dea4e4d04c9dea
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-17 21:24:40 +0200

    Add recursion to `factorial`

--- factorial/src/main.cpp
+++ factorial/src/main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 long long factorial(int x)
 {
@@ -10,9 +11,18 @@ long long factorial(int x)
     return output;
 }
 
+long long factorial_recursion(int x)
+{
+    if (x == 1) return x;
+    else return x * factorial_recursion(x - 1);
+}
+
 int main()
 {
     int x;
-    scanf("%i", &x);
-    printf("%lli\n", factorial(x));
+    cin >> x;
+
+    // you can see that this returns the same value no matter what we use (iteration or recursion)
+    cout << factorial(x) << endl;
+    cout << factorial_recursion(x) << endl;
 }
