d73809b26e29de58938ab7650d4bfc8187f02574
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-17 21:25:16 +0200

    Simplify `triangle` solution

--- triangle/src/main.cpp
+++ triangle/src/main.cpp
@@ -1,26 +1,20 @@
-#include <stdio.h>
-#include <numeric>
+#include <iostream>
+using namespace std;
 
 int main()
 {
     int a, b, c;
-    scanf("%i %i %i", &a, &b, &c);
+    cin >> a >> b >> c;
 
-    bool conditions[3] = {
-        a + b > c,
-        a + c > b,
-        b + c > a
-    };
-    
-    int check = std::accumulate(conditions, conditions + sizeof(conditions), 0);
-    
-    if (check == sizeof(conditions)) {
-        printf("All conditions are true. Triangle can be built.");
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        cout << "All conditions are true. Triangle can be built.";
     }
-    else {
-        printf("One or more condition(s) failed. Triangle cannot be built.");
+    else
+    {
+        cout << "One or more condition(s) failed. Triangle cannot be built.";
     }
 
-    printf("\n");
+    cout << endl;
     return 0;
 }
